c0092904d7d5d01a2711fbfdf492ed9a78fdfa03
marwan hp <marwan-hp@gitlab.ms>
2021-07-03 06:15:28 +0300

    3D animation

---
+++ lab10/MyAnimation.java
@@ -0,0 +1,109 @@
+package lab10;
+
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.GLUT;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+/**
+ * 3D Transformation
+ * @author Marwan
+ */
+public class MyAnimation implements GLEventListener {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Lab 10 3D Animation");
+        GLCanvas canvas = new GLCanvas();
+
+        canvas.addGLEventListener(new MyAnimation());
+        frame.add(canvas);
+        frame.setSize(680, 480);
+        final Animator animator = new Animator(canvas);
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        animator.stop();
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        // Center frame
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        animator.start();
+    }
+
+    public void init(GLAutoDrawable drawable) {
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
+        GL gl = drawable.getGL();
+        glut = new GLUT();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+
+        // Setup the drawing area and shading mode
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+        
+            height = 1;
+        }
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, h, 1.0, 20.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    float angle = 0.0f;
+    GLUT glut;
+    
+    public void display(GLAutoDrawable drawable) {
+        GL gl = drawable.getGL();
+
+        // Clear the drawing area
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+
+        // Move the "drawing cursor" around
+        gl.glTranslatef(0.0f, 0.0f, -6.5f); // setup view to make models visiable
+        
+        angle += 0.2f;
+        if (angle >= 360.0f) angle = 0.0f;
+        
+        gl.glRotatef(angle, 0.0f, 1.0f, 0.0f);
+        glut.glutWireTeapot(1.0);
+
+        // Flush all drawing operations to the graphics card
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+}
---
+++ lab10/README.md
@@ -0,0 +1,11 @@
+# 3D Animation
+
+Computer animation is the art of creating moving images via the use of computers. It is a subfield of computer graphics and animation.
+
+## Applying 3D animation
+
+First you need to define variable as `class` attribute. For instance, if we want to create animation by rotating an object, we need to define `angle` variable with initial value. Then in `display()` method, we change that angle as required by increasing or decreasing. Then apply rotation using update angle. This is can be applied to translation, scaling or any kind of drawing. As long as `display()` method called it will result different action.
+
+### Sample Run
+![Rotating a Teapot](/images/lab10/rotate-teapot.gif)
+
